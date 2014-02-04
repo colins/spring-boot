df1885a3edaae7c56acc0144237302e49e9a574c
Colin Shield <pair+colin@pivotallabs.com>
2014-02-04 14:16:48 -0500

    fixing the app server

--- src/main/java/com/springapp/mvc/UserController.java
+++ src/main/java/com/springapp/mvc/UserController.java
@@ -3,17 +3,11 @@ package com.springapp.mvc;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.*;
-
-
 
 @Controller
 public class UserController {
